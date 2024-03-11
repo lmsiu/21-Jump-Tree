53be21b14260bd8a134683d691013c933278c4fc
dury2379 <dennis.uryeff@gmail.com>
2024-03-11 03:21:52 -0700

    Added Bullets

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public float ttl;
+    public GameObject owner;
+    public bool isPlayer;
+    public float damage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    public void startSelfDestruct()
+    {
+       StartCoroutine(Disappear()); 
+    }
+
+    IEnumerator Disappear()
+    {
+        yield return new WaitForSeconds(ttl);
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerController : PhysicsBase
 {
+    private bool isMouseDragging = false;
+    private Vector2 startPoint;
+    private Vector2 dragVector;
+    public int maxPowerDrag = 50;
+    public GameObject bullet;
+    public float maxBulletSpeed = 40f;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,5 +24,44 @@ public class PlayerController : PhysicsBase
         if (Input.GetAxis("Horizontal") < 0) desiredx = -3;
 
         if (Input.GetButton("Jump") && grounded) velocity.y = 6.5f;
+        
+        
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            isMouseDragging = true;
+            startPoint = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (dragVector.magnitude != 0.0)
+            {
+                shoot(-dragVector);
+            }
+            isMouseDragging = false;
+            dragVector = new Vector2(0, 0);
+            startPoint = new Vector2(-1, -1);
+        }
+
+        if (isMouseDragging)
+        {
+            dragVector = new Vector2(Input.mousePosition.x, Input.mousePosition.y) - startPoint;
+        }
     }
+    
+    void shoot(Vector2 direction)
+    {
+        Vector2 velocity = direction.normalized * (dragVector.magnitude / maxPowerDrag) * maxBulletSpeed;
+        print(transform.position);
+    	GameObject b = Instantiate(bullet, transform.position + (new Vector3(direction.normalized.x, direction.normalized.y, 0) * 0.7f), Quaternion.Euler(0, 0, 0));
+    	b.GetComponent<Rigidbody2D>().velocity = velocity;
+    	BulletController bc = b.GetComponent<BulletController>();
+    	bc.damage = 20;
+    	bc.isPlayer = true;
+    	bc.ttl = 3f;
+    	bc.owner = gameObject;
+    	bc.startSelfDestruct();
+    }
+    
 }
