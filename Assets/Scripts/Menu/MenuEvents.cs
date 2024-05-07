e6aaa1fee814e2badc1920a5c5f7d55c8e78638c
E-voni <63889874+E-voni@users.noreply.github.com>
2024-05-07 03:05:41 -0700

    added volume control

--- Assets/Scripts/Menu/MenuEvents.cs
+++ Assets/Scripts/Menu/MenuEvents.cs
@@ -1,8 +1,20 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
 
 public class MenuEvents : MonoBehaviour
 {
+    public Slider VolumeSlider;
+    public AudioMixer mixer;
+
+    public void SetVolume()
+    {
+        mixer.SetFloat("volume", VolumeSlider.value);
+        
+    }
+
     public void LoadLevel(int index)
     {
         SceneManager.LoadScene(index);
